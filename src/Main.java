ff7f093906d3c17e6c181522b66b4cc3a23ff475
Sergey2875 <s_grigorenko@mail.ru>
2025-06-22 13:24:29 +0300

    Домашнее задание 2

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,43 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        var dog = 8;
+        var dog = 8.0;
         System.out.println(dog);
+        var cat = 3.6;
+        System.out.println(cat);
+        var paper = 763789;
+        System.out.println(paper);
+        dog = dog + 4;
+        System.out.println(dog);
+        cat = cat + 4;
+        System.out.println(cat);
+        paper = paper + 4;
+        System.out.println(paper);
+        dog = dog - 3.5;
+        System.out.println(dog);
+        cat = cat - 1.6;
+        System.out.println(cat);
+        paper = paper - 7639;
+        System.out.println(paper);
+        var friend =  19;
+        System.out.println(friend);
+        friend = friend + 2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
+        var frog = 3.5;
+        System.out.println(frog);
+        frog = frog * 10;
+        System.out.println(frog);
+        frog = frog + 4;
+        System.out.println(frog);
+        var weithBoxer1 = 78.2;
+        System.out.println(weithBoxer1);
+        var weithBoxer2 = 82.7;
+        System.out.println(weithBoxer2);
+        var totalWeithBoxers = weithBoxer1 + weithBoxer2;
+        System.out.println(totalWeithBoxers);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+
     }
 }
